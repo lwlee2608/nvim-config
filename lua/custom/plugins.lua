632427a5d24b67ec69e2a9adaf59ccda3ec54bf5
Jason Lee <lwlee2608@gmail.com>
2026-02-05 15:44:59 +0800

    add ThePrimeagen/99 plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -44,5 +44,34 @@ local plugins = {
       vim.g.rustfmt_autosave = 1
     end
   },
+  {
+    "ThePrimeagen/99",
+    lazy = false,
+    config = function()
+      local _99 = require("99")
+      local providers = require("99.providers")
+      local cwd = vim.uv.cwd()
+      local basename = vim.fs.basename(cwd)
+      _99.setup({
+        provider = providers.OpenCodeProvider,
+        model = "openrouter/moonshotai/kimi-k2.5",
+        logger = {
+          level = _99.DEBUG,
+          path = "/tmp/" .. basename .. ".99.debug",
+          print_on_error = true,
+        },
+        completion = {
+          source = "cmp",
+        },
+        md_files = {
+          "AGENT.md",
+        },
+      })
+
+      vim.keymap.set("n", "<leader>9f", function() _99.fill_in_function() end)
+      vim.keymap.set("v", "<leader>9v", function() _99.visual() end)
+      vim.keymap.set("v", "<leader>9s", function() _99.stop_all_requests() end)
+    end,
+  },
 }
 return plugins
