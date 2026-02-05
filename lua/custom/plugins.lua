85c617987c75be3ac135b7402eb2bb077b589ddb
Jason Lee <lwlee2608@gmail.com>
2026-02-05 12:48:56 +0800

    install 99 plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -57,5 +57,33 @@ local plugins = {
       vim.g.rustfmt_autosave = 1
     end
   },
+  {
+    dir = "/Users/lwlee2608/src/github/99",
+    lazy = false,
+    config = function()
+      local _99 = require("99")
+      local providers = require("99.providers")
+      local cwd = vim.uv.cwd()
+      local basename = vim.fs.basename(cwd)
+      _99.setup({
+        provider = providers.ClaudeCodeProvider,
+        logger = {
+          level = _99.DEBUG,
+          path = "/tmp/" .. basename .. ".99.debug",
+          print_on_error = true,
+        },
+        completion = {
+          source = "cmp",
+        },
+        md_files = {
+          "AGENT.md",
+        },
+      })
+
+      vim.keymap.set("n", "<leader>9f", function() _99.fill_in_function() end)
+      vim.keymap.set("v", "<leader>9v", function() _99.visual() end)
+      vim.keymap.set("v", "<leader>9s", function() _99.stop_all_requests() end)
+    end,
+  },
 }
 return plugins
