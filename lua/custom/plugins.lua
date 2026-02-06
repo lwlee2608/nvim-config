9371474b73db43c187b14ac3e7559af07c0ac08a
Jason Lee <lwlee2608@gmail.com>
2026-02-06 12:08:16 +0800

    add shortcut for visual prompt

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -69,7 +69,9 @@ local plugins = {
       })
 
       vim.keymap.set("n", "<leader>9f", function() _99.fill_in_function() end)
+      vim.keymap.set("n", "<leader>9p", function() _99.fill_in_function_prompt() end)
       vim.keymap.set("v", "<leader>9v", function() _99.visual() end)
+      vim.keymap.set("v", "<leader>9p", function() _99.visual_prompt() end)
       vim.keymap.set("v", "<leader>9s", function() _99.stop_all_requests() end)
     end,
   },
