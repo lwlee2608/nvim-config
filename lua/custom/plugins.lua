a8090b55a23bb1058c06a6a1f28a3df9a3de676a
Jason Lee <lwlee2608@gmail.com>
2026-09-21 11:25:04 +0800

    remove 99 plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -70,35 +70,5 @@ local plugins = {
       { "<leader>mr", "<cmd>RenderMarkdown toggle<CR>", desc = "Toggle render markdown" },
     },
   },
-  {
-    "ThePrimeagen/99",
-    lazy = false,
-    config = function()
-      local _99 = require("99")
-      local providers = require("99.providers")
-      local cwd = vim.uv.cwd()
-      local basename = vim.fs.basename(cwd)
-      _99.setup({
-        provider = providers.OpenCodeProvider,
-        model = "openrouter/moonshotai/kimi-k2.5",
-        -- model = "openrouter/anthropic/claude-sonnet-4.5",
-        logger = {
-          level = _99.DEBUG,
-          path = "/tmp/" .. basename .. ".99.debug",
-          print_on_error = true,
-        },
-        completion = {
-          source = "cmp",
-        },
-        md_files = {
-          "AGENT.md",
-        },
-      })
-
-      vim.keymap.set("n", "<leader>9f", function() _99.fill_in_function() end)
-      vim.keymap.set("v", "<leader>9v", function() _99.visual() end)
-      vim.keymap.set("v", "<leader>9s", function() _99.stop_all_requests() end)
-    end,
-  },
 }
 return plugins
